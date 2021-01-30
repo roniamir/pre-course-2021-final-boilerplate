b7863a42a208074336f21cbc7b81d9db37757818
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-30 17:54:29 +0200

    Fixed list "bad way" div
    Changed the new ekement we add to the list to div elements, as README file say

--- src/main.js
+++ src/main.js
@@ -1,33 +1,30 @@
 
-const addButton = document.getElementById('add-button');
+const addButton = document.getElementById('add-button'); 
 const input = document.getElementById('text-input');
 const prioritySelect = document.getElementById('priority-selector');
-
 const todoContainer = document.getElementById('list-container');
-console.log(addButton);
-console.log(input);
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', () => { //add new item to the list
     const insertedValue = input.value;  //get the user new todo content
     const currentPriority = prioritySelect.value; //get the nte todo's priority
 
-    console.log(currentPriority);
    
-    if(insertedValue !== ''){
+    if(insertedValue !== '' && insertedValue !== undefined && currentPriority){ //if the user inserted his new todo and choose the item priority, then add to to the list
+        console.log("the value is : "+insertedValue);
         let newDivLi =  document.createElement('div');
         newDivLi.setAttribute('class','todo-container');
 
-        const itemPriorety = document.createElement('span');
+        const itemPriorety = document.createElement('div');
         itemPriorety.innerText = currentPriority;
 console.log(itemPriorety);
 
-        const itemText = document.createElement('p');
+        const itemText = document.createElement('div');
         itemText.setAttribute('class', 'todo-text');
         itemText.innerText = insertedValue;
 console.log(itemText);
 
         
-        const itemAddTime = document.createElement('p');
+        const itemAddTime = document.createElement('div');
         itemAddTime.setAttribute('class', 'todo-created-at');
         itemAddTime.innerText = new Date().toLocaleString();
 console.log(itemAddTime);
