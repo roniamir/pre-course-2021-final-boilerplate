542d8b34690baca299332f7b13b937ea5593dc08
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-30 18:47:01 +0200

    add check priority function
    return if the current priority exist. If so, the new todo wont be in the list

--- src/main.js
+++ src/main.js
@@ -4,30 +4,37 @@ const input = document.getElementById('text-input');
 const prioritySelect = document.getElementById('priority-selector');
 const todoContainer = document.getElementById('list-container');
 
+let todoCounter = 0;
+const usedPrioritys =[];
+
 addButton.addEventListener('click', () => { //add new item to the list
     const insertedValue = input.value;  //get the user new todo content
     const currentPriority = prioritySelect.value; //get the nte todo's priority
 
-   
-    if(insertedValue !== '' && insertedValue !== undefined && currentPriority){ //if the user inserted his new todo and choose the item priority, then add to to the list
-        console.log("the value is : "+insertedValue);
+    const isNewPriorityExist = isPriorityExist(currentPriority, usedPrioritys);  //check if the user already choose the current priorety
+    
+
+    if(insertedValue !== '' && insertedValue !== undefined && currentPriority && !isNewPriorityExist){ //if the user inserted good todo: 1-got value 2-choosed priority 3-the prioriry has not choose before
+        
+        usedPrioritys.push(currentPriority); //push the new priority to the counter (priority array)
+        
         let newDivLi =  document.createElement('div');
         newDivLi.setAttribute('class','todo-container');
 
         const itemPriorety = document.createElement('div');
         itemPriorety.innerText = currentPriority;
-console.log(itemPriorety);
+//console.log(itemPriorety);
 
         const itemText = document.createElement('div');
         itemText.setAttribute('class', 'todo-text');
         itemText.innerText = insertedValue;
-console.log(itemText);
+//console.log(itemText);
 
         
         const itemAddTime = document.createElement('div');
         itemAddTime.setAttribute('class', 'todo-created-at');
         itemAddTime.innerText = new Date().toISOString().slice(0, 19).replace('T', ' ');
-console.log(itemAddTime);
+//console.log(itemAddTime);
 
 
         newDivLi.append(itemPriorety);
@@ -36,8 +43,19 @@ console.log(itemAddTime);
         
         todoContainer.append(newDivLi);
 
-         
-    console.log(newDivLi);
+        todoCounter++;
+        const counterText = document.getElementById('counter');
+        counterText.innerText = "You have got " +todoCounter + " to-do" ;
     }
   
-})
+});
+
+function isPriorityExist (priority, arr){
+    for(let priorityNum of arr){
+        if(priority === priorityNum){
+            return true;
+        } 
+    }
+    return false;
+}
+
