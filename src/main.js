b890c54f5b68ab55ad435d01feb63b43f65d12e0
roniamir <77384841+roniamir@users.noreply.github.com>
2021-02-01 21:17:57 +0200

    new background design
    all tests are working! first edit to background style- blue sky and sun

--- src/main.js
+++ src/main.js
@@ -4,30 +4,40 @@ const input = document.getElementById('text-input');
 const prioritySelect = document.getElementById('priority-selector');
 //const todoContainer = document.getElementById('list-container');
 const viewSection = document.getElementById('view-section');
-
+const data = []
 const counterText = document.getElementById('counter');
-let todoCounter = 0;
-counterText.innerText = todoCounter;
+const sortButton = document.getElementById('sort-button');
 
-//console.log(typeof todoListArray === 'undefined');
+let todoListArray = JSON.parse(localStorage.getItem('todo-list'));
+let usedPrioritys = JSON.parse(localStorage.getItem('used=priority-list'));
+let todoCounter;
 
-//const todoListArray = JSON.parse(localStorage.getItem('todo-list'));
-
-if(typeof todoListArray === 'undefined'){
-    let usedPrioritys = [];
-    let todoListArray = [];
+if(!todoListArray){
+    todoListArray= [];
+    usedPrioritys = [];
     localStorage.setItem( 'todo-list', JSON.stringify(todoListArray) ); 
     localStorage.setItem('used-priorities-list', JSON.stringify(todoListArray));
-} //else{
-  //  let todoListArray = JSON.parse(localStorage.getItem('todo-list'));
-    //listToDom();
-//}
+    todoCounter = 0;
+    counterText.innerText = todoCounter;
+} else{
+    loadTodos();
+}
     
-
+//Event listeners
 addButton.addEventListener('click', () => { //add new item to the list
     creatNewTodo();
 });
 
+sortButton.addEventListener('click', () => {
+    if(!isListSorted(todoListArray)){
+       todoListArray = sortTodoList(); 
+       let oldOrder = document.querySelectorAll('.todo-container');
+       oldOrder.forEach(element => {
+           element.remove();
+       });
+        loadTodos();
+    }
+})
 function isPriorityExist (priority, arr){
     for(let obj of arr){
         console.log(obj.priority+" is the priority in the array");
@@ -42,95 +52,57 @@ function isPriorityExist (priority, arr){
 function creatNewTodo(){
     const insertedValue = input.value;  //get the user new todo content
     const currentPriority = prioritySelect.value; //get the nte todo's priority
-    //const usedPrioritysArray = JSON.parse(localStorage.getItem('used-priorities-list'));
-    let todoListArray = JSON.parse(localStorage.getItem('todo-list'));
-    let usedPrioritys = JSON.parse(localStorage.getItem('used-priorities-list'));
+    usedPrioritys = JSON.parse(localStorage.getItem('used-priorities-list'));
     
-    const isNewPriorityExist = isPriorityExist(currentPriority, todoListArray);  //check if the user already choose the current priorety
-    if(insertedValue !== '' && insertedValue !== undefined && currentPriority && !isNewPriorityExist){ //if the user inserted good todo: 1-got value 2-choosed priority 3-the prioriry has not choose before
-        usedPrioritys.push(currentPriority); //push the new priority to the counter (priority array)
-        localStorage.setItem('used-priorities-list',JSON.stringify(usedPrioritys));
-
+   // const isNewPriorityExist = isPriorityExist(currentPriority, usedPrioritys);  //check if the user already choose the current priorety
+   //+ on the if(!isNewPriorityExist)...
+    if(insertedValue !== '' && insertedValue !== undefined && currentPriority){ //if the user inserted good todo: 1-got value 2-choosed priority 3-the prioriry has not choose before
+        usedPrioritys.push(currentPriority); 
+        console.log('add new todo, the used priorities are: ' + usedPrioritys);//push the new priority to the counter (priority array)
+        localStorage.setItem('used-priorities-list', JSON.stringify(usedPrioritys));
+        
         const todoObj = { //creat todo object 
             text: insertedValue,
             priority: currentPriority,
             date: new Date().toISOString().slice(0, 19).replace('T', ' ')
         }
         
-        let todoListArray = JSON.parse(localStorage.getItem('todo-list'));
-        todoListArray.push(todoObj);
-        localStorage.setItem('todo-list', JSON.stringify(todoListArray));
-        
-        let newDivLi =  document.createElement('div');
-        newDivLi.setAttribute('class','todo-container');
+                    todoListArray = JSON.parse(localStorage.getItem('todo-list'));
+                    todoListArray.push(todoObj);
+                    localStorage.setItem('todo-list', JSON.stringify(todoListArray));
+                    
+                    let newDivLi =  document.createElement('div');
+                    newDivLi.setAttribute('class','todo-container');
 
-        const itemPriorety = document.createElement('div');
-        itemPriorety.setAttribute('class', 'todo-priority todo-item');
-        itemPriorety.innerText = todoObj.priority;
+                    const itemPriorety = document.createElement('div');
+                    itemPriorety.setAttribute('class', 'todo-priority todo-item');
+                    itemPriorety.innerText = todoObj.priority;
 
-        const itemText = document.createElement('div');
-        itemText.setAttribute('class', 'todo-text todo-item');
-        itemText.innerText = todoObj.text;
-        
-        const itemAddTime = document.createElement('div');
-        itemAddTime.setAttribute('class', 'todo-created-at todo-item');
-        itemAddTime.innerText = todoObj.date;
-
-
-        newDivLi.append(itemPriorety);
-        newDivLi.append(itemText);
-        newDivLi.append(itemAddTime);
-        
-        viewSection.append(newDivLi);
-
-        todoCounter++;
-        counterText.innerText = todoCounter;
-        input.value = '';
-    }
-}
-
-function listToDom(){
-    for(let todoObj of todoListArray){
-        let newDivLi =  document.createElement('div');
-        newDivLi.setAttribute('class','todo-container');
-
-        const itemPriorety = document.createElement('div');
-        itemPriorety.setAttribute('class', 'todo-priority todo-item');
-        itemPriorety.innerText = todoObj.priority;
-
-        const itemText = document.createElement('div');
-        itemText.setAttribute('class', 'todo-text todo-item');
-        itemText.innerText = todoObj.text;
-    
-        const itemAddTime = document.createElement('div');
-        itemAddTime.setAttribute('class', 'todo-created-at todo-item');
-        itemAddTime.innerText = todoObj.date;
+                    const itemText = document.createElement('div');
+                    itemText.setAttribute('class', 'todo-text todo-item');
+                    itemText.innerText = todoObj.text;
+                    
+                    const itemAddTime = document.createElement('div');
+                    itemAddTime.setAttribute('class', 'todo-created-at todo-item');
+                    itemAddTime.innerText = todoObj.date;
 
 
-        newDivLi.append(itemPriorety);
-        newDivLi.append(itemText);
-        newDivLi.append(itemAddTime);
-        
-        viewSection.append(newDivLi);
+                    newDivLi.append(itemPriorety);
+                    newDivLi.append(itemText);
+                    newDivLi.append(itemAddTime);
+                    
+                    viewSection.append(newDivLi);
 
-        todoCounter++;
-        counterText.innerText = todoCounter;
-        input.value = '';
+                    todoCounter = todoListArray.length;
+                    counterText.innerText = todoCounter;
+                    input.value = '';
     }
 }
 
 
-const sortButton = document.getElementById('sort-button');
 
-sortButton.addEventListener('click', () => {
-    let todoListArray = JSON.parse(localStorage.getItem('todo-list'));
-    let usedPrioritys = JSON.parse(localStorage.getItem('used-priorities-list'));
 
-    if(!isListSorted(todoListArray)){
-       todoListArray = sortTodoList(todoListArray, usedPrioritys); 
-       showSortListInDom(todoListArray);
-    }
-})
+
 
 //check if the to-do list is sorted
 function isListSorted(arr){
@@ -146,31 +118,23 @@ function isListSorted(arr){
 }
 
 //sort the to-do list
-function sortTodoList(todoArr, prioretyArr){
-    let newOrderPriority = prioretyArr;
+function sortTodoList(){
+    let newOrderPriority = JSON.parse(localStorage.getItem('used-priorities-list'));
     newOrderPriority.sort((a, b) =>  b - a);  //change the order of the priorities from the largest to the smallest
-    let sortTodoList = [];
+    let sortedTodoList = [];
     for(let i = 0; i < newOrderPriority.length; i++){
-        for(let obj of todoArr){
-
+        for(let obj of todoListArray){
             if(obj.priority === newOrderPriority[i]){
-                sortTodoList.push(obj);
+                sortedTodoList.push(obj);
                 break;
             }
         }
     }
-    return sortTodoList;
+    return sortedTodoList;
 }
 
-function showSortListInDom(arr){
-    const allMyTodo = document.querySelectorAll('.todo-container');
-    for(let i = 0; i < allMyTodo.length; i++){
-        allMyTodo[i].remove();
-    }
-   // localStorage.setItem('todo-list', JSON.stringify(todoListArray));
-    
-
-    for(let todoObj of arr){
+function loadTodos(){
+    for(let todoObj of todoListArray){
               
         let newDivLi =  document.createElement('div');
         newDivLi.setAttribute('class','todo-container');
@@ -191,8 +155,10 @@ function showSortListInDom(arr){
         newDivLi.append(itemText);
         newDivLi.append(itemAddTime);
         
-        viewSection.append(newDivLi);
-    }   
+        viewSection.append(newDivLi); 
+    }
+    todoCounter = todoListArray.length;
+    counterText.innerText = todoCounter;
 }
 
 
